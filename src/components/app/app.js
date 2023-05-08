f553519eac46cdfd371a69f7817caa136b0eb670
KovOlga <switch.to.olga@yandex.ru>
2023-05-08 20:49:38 +0300

    added datePicker and time select

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,22 @@
 import styles from "./app.module.css";
 import { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   const defaultFloorList = Array.from(Array(25), (_, i) => i + 3);
   const defaultConfRoomList = Array.from(Array(10), (_, i) => i + 1);
+  const defaultTimeList = [];
+  const createDefaultTimeList = () => {
+    for (let i = 9; i <= 18; i++) {
+      defaultTimeList.push(`${i}:00`);
+      defaultTimeList.push(`${i}:30`);
+    }
+
+    // console.log(hours);
+  };
+
+  createDefaultTimeList();
 
   const [textAreaState, setTextAreaState] = useState("");
 
@@ -21,7 +34,11 @@ function App() {
       tower: e.target.elements.tower.value,
       floor: e.target.elements.floor.value,
       confRoom: e.target.elements.confRoom.value,
-      // data: e.target.elements.data.value,
+      data: date,
+      time: {
+        from: e.target.elements.timeFrom.value,
+        to: e.target.elements.timeTo.value,
+      },
       comment: e.target.elements.comment.value,
     };
     console.log(JSON.stringify(newBooking));
@@ -48,6 +65,18 @@ function App() {
   };
 
   const [confRoomList, setConfRoomList] = useState(defaultConfRoomList);
+
+  const [date, setStartDate] = useState(new Date());
+
+  const [time, setTime] = useState({ from: "9:00", to: "9:00" });
+
+  const handleTimeFromSelect = (e) => {
+    setTime({ ...time, from: e.target.value });
+  };
+
+  const handleTimeToSelect = (e) => {
+    setTime({ ...time, to: e.target.value });
+  };
 
   return (
     <div className="App">
@@ -96,7 +125,46 @@ function App() {
           </select>
         </div>
 
-        <h2>Дата</h2>
+        <div>
+          <h2>Дата</h2>
+
+          <DatePicker
+            selected={date}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="dd-MM-yyyy"
+          />
+        </div>
+
+        <div>
+          <h2>Время</h2>
+          <h3>From</h3>
+          <select
+            name="timeFrom"
+            value={time.from}
+            onChange={handleTimeFromSelect}
+          >
+            {defaultTimeList.length > 0 &&
+              defaultTimeList.map((fromTime, i) => {
+                return (
+                  <option key={i} value={fromTime}>
+                    {fromTime}
+                  </option>
+                );
+              })}
+          </select>
+
+          <h3>To</h3>
+          <select name="timeTo" value={time.to} onChange={handleTimeToSelect}>
+            {defaultTimeList.length > 0 &&
+              defaultTimeList.map((toTime, i) => {
+                return (
+                  <option key={i} value={toTime}>
+                    {toTime}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
 
         <div>
           <h2>Поле для комментария</h2>
